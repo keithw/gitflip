a5c19efdcc213470ab00c58098cd4f620f74c343
Keith Winstein <keithw@mit.edu>
2010-06-05 01:15:06 -0400

    Use mmap flag to populate page table

--- file.cpp
+++ file.cpp
@@ -24,19 +24,11 @@ File::File( char *filename )
   filesize = thestat.st_size;
 
   /* Map file */
-  buffer = (uint8_t *)mmap( NULL, filesize, PROT_READ, MAP_SHARED, fd, 0 );
+  buffer = (uint8_t *)mmap( NULL, filesize, PROT_READ, MAP_SHARED | MAP_POPULATE, fd, 0 );
 
   if ( buffer == MAP_FAILED ) {
     perror( "mmap" );
     throw UnixError( errno );
-  }  
-
-  /* Preload contents to help Linux VM */
-  /* It's unfortunate this is a speedup */
-
-  volatile uint8_t sum = 0;
-  for ( int i = 0; i < filesize; i++ ) {
-    sum |= buffer[ i ];
   }
 }
 
